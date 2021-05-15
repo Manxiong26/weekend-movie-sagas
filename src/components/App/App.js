7a5608b1d292694cd4b2d770927e3279d943771f
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-14 21:08:59 -0500

    created a new component called AddMovie and put some inputs on the DOM with the link to the AddMove page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,24 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import Detail from '../Detail/Detail'
+import AddMovie from '../AddMovie/AddMovie'
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
       <Router>        
+          <nav>
+              <p><Link to="/AddMovie">Add Movie</Link></p>
+          </nav>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        <Route path="/detail" exct>
+        <Route path="/AddMovie" exact>
+          <AddMovie />
+        </Route>
+        <Route path="/detail" exact>
         <Detail />
         </Route>
         {/* Details page */}
--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 
 function Detail(){
@@ -9,10 +9,15 @@ function Detail(){
     return(
         <main>
             <h1>MovieList</h1>
-            <section className="movies">
+            <section className="details">
                 {movies.map(movie => {
                     return (
                         <div>
+                            <Router>
+                                <nav>
+                                    <p><Link to="/">Home</Link></p>
+                                </nav>
+                            </Router>
                             <h3>{movie.description}</h3>
                             <img src={movie.poster} alt={movie.description}/>
                         </div>
